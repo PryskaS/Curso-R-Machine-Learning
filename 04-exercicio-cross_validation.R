3e0d6ef9a138df75126a607e7e76301ed16f3077
PryskaS <60328960+PryskaS@users.noreply.github.com>
2020-05-20 14:47:12 -0300

    estratégia de validação cruzada

---
+++ 04-exercicio-cross_validation.R
@@ -0,0 +1,98 @@
+# Pacotes ------------------------------------------------------------------
+
+library(tidymodels)
+library(parsnip)
+library(ISLR)
+library(rsample)
+library(dials)
+library(yardstick)
+
+# Me ----------------------------------------------------------------------
+
+todos_os_dados <- bind_rows(dados1, dados2, dados3, dados4)
+
+quebra_5p <- vfold_cv(todos_os_dados, v = 5)
+# MC CV serve para termos o treino e o teste dentro do mesmo objeto
+
+especificacao_arvore_n_variavel <- decision_tree(min_n = tune()) %>%
+  # A especificação é igual, mas se queremos que a f possa ter qualquer n, e não um fixo,
+  # usamos a função tune()
+  set_mode("regression") %>%
+  set_engine("rpart")
+
+# Us ---------------------------------------------------------------------
+
+# deixando o tune_grid() sortear valores para os hiperparâmetros
+tune <- tune_grid(
+  # quando for fitar vários modelos, você deve usar a função tune_grid
+  y ~ x,
+  especificacao_arvore_n_variavel,
+  grid = 20,
+  # o parâmetro levels define em quantos pontos queremos fazer a conta
+  resamples = quebra_5p,
+  metrics = metric_set(rmse, mae, mape, mase)
+)
+
+autoplot(tune)
+
+
+# usando lista de parâmetros explicitamente com as funções parameters() + update() + min_n(range =) + grid_regular
+pars <- parameters(especificacao_arvore_n_variavel) %>% 
+  update(min_n = dials::min_n(range = c(10, 40))) %>%
+  grid_regular(levels = 4)
+# precisamos trocar os XXX pelo range de hiperparâmetros que queremos procurar
+
+tune <- tune_grid(
+  # quando for fitar vários modelos, você deve usar a função tune_grid
+  y ~ x,
+  especificacao_arvore_n_variavel,
+  grid = pars,
+  # o parâmetro levels define em quantos pontos queremos fazer a conta
+  resamples = quebra_5p,
+  metrics = metric_set(rmse, mae, mape, mase)
+)
+
+autoplot(tune)
+
+# You ---------------------------------------------------------------------
+
+# 1. Encontre o melhor min_n para a base Hitters.
+especificacao_arvore_n_variavel <- decision_tree(min_n = tune(),
+                                                 cost_complexity = tune()) %>%
+  set_mode("regression") %>%
+  set_engine("rpart")
+
+#quebra_5fold <- vfold_cv(Hitters, v = 5)
+quebra_mc <- mc_cv(Hitters, prop = .7, times = 2)
+
+tunado <- tune_grid(
+  HmRun ~ CHmRun,
+  especificacao_arvore_n_variavel,
+  grid = 20,
+  resamples = quebra_mc,
+  metrics = metric_set(rmse, mae)
+)
+
+autoplot(tunado)
+
+# 2. [Extra] Encontra o melhor min_n para a base Hitters, mas fazendo um modelo que
+# tem todas as variáveis históricas dos jogadores (elas começam com C maiúsculo).
+# Lembre-se: se você quiser usar mais variáveis no seu modelo de árvore,
+# basta fazer x+y+z quando for especificar a formula.
+
+especificacao_arvore_n_variavel <- decision_tree(min_n = tune()) %>%
+  set_mode("regression") %>%
+  set_engine("rpart")
+
+quebra_5fold <- vfold_cv(Hitters, v = 5)
+
+tunado <- tune_grid(
+  HmRun ~ CHmRun+CAtBat+CHits+CRuns+CRBI+CWalks,
+  especificacao_arvore_n_variavel,
+  grid = 10,
+  resamples = quebra_5fold,
+  metrics = metric_set(rmse, mae)
+)
+
+autoplot(tunado)
+
